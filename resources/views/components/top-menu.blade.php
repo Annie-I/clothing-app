282a051a0ac9e86602fe153ca41fa6c86f64dcf7
Annie-I <annija.iljina@gmail.com>
2021-10-10 17:28:39 +0300

    head and nav bar components

---
+++ resources/views/components/top-menu.blade.php
@@ -0,0 +1,52 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+    <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Sākums</a>
+                </li>
+                <div class="vr"></div>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Apģērbi un aksesuāri
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Sievietēm</a></li>
+                        <li><a class="dropdown-item" href="#">Vīriešiem</a></li>
+                        <li><a class="dropdown-item" href="#">Bērniem</a></li>
+                    </ul>
+                </li>
+                <div class="vr"></div>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Mājai
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Mēbeles</a></li>
+                        <li><a class="dropdown-item" href="#">Tehnika</a></li>
+                    </ul>
+                </li>
+                <div class="vr"></div>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dārzam
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Siltumnīcai</a></li>
+                        <li><a class="dropdown-item" href="#">Stādi</a></li>                        
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Rādīt visu</a></li>
+                    </ul>
+                </li>
+                <div class="vr"></div>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Kontakti</a>
+                </li>
+                <div class="vr"></div>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pierakstīties</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
