a9d28b56225f2c83c9c307ec8fb57b2648f275a3
Annie-I <annija.iljina@gmail.com>
2021-10-19 00:19:35 +0300

    nav bar v2 + empty page body

--- resources/views/components/top-menu.blade.php
+++ resources/views/components/top-menu.blade.php
@@ -1,85 +1,78 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-transparent">
-    <div class="container-fluid">
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Sievietēm
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Aksesuāri</a></li>
-                        <li><a class="dropdown-item" href="#">Apavi</a></li>
-                        <li><a class="dropdown-item" href="#">Apģērbs</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Citi</a></li>
-                    </ul>
-                </li>
-                <div class="vr"></div>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Vīriešiem
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Aksesuāri</a></li>
-                        <li><a class="dropdown-item" href="#">Apavi</a></li>
-                        <li><a class="dropdown-item" href="#">Apģērbs</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Citi</a></li>
-                    </ul>
-                </li>
-                <div class="vr"></div>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Bērniem
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Aksesuāri</a></li>
-                        <li><a class="dropdown-item" href="#">Apavi</a></li>
-                        <li><a class="dropdown-item" href="#">Apģērbs</a></li>
-                        <li><a class="dropdown-item" href="#">Rotaļlietas</a></li>
-                        <li><a class="dropdown-item" href="#">Skolai</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Citi</a></li>
-                    </ul>
-                </li>
-                <div class="vr"></div>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Mājai
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Dekori</a></li>
-                        <li><a class="dropdown-item" href="#">Grāmatas</a></li>
-                        <li><a class="dropdown-item" href="#">Interjera priekšmeti</a></li>
-                        <li><a class="dropdown-item" href="#">Mēbeles</a></li>
-                        <li><a class="dropdown-item" href="#">Remontam</a></li>
-                        <li><a class="dropdown-item" href="#">Tehnika</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Citi</a></li>
-                    </ul>
-                </li>
-                <div class="vr"></div>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dārzam
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Instrumenti</a></li>                        
-                        <li><a class="dropdown-item" href="#">Siltumnīcai</a></li>
-                        <li><a class="dropdown-item" href="#">Stādi</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Citi</a></li>
-                    </ul>
-                </li>
-                <div class="vr"></div>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Kontakti</a>
-                </li>
-                <div class="vr"></div>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Pierakstīties</a>
-                </li>
+<nav class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+    <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+    <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+    </a>
+
+    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li class="nav-item dropdown">
+            <a class="nav-link link-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Sievietēm
+            </a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Aksesuāri</a></li>
+                <li><a class="dropdown-item" href="#">Apavi</a></li>
+                <li><a class="dropdown-item" href="#">Apģērbs</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Citi</a></li>
             </ul>
-        </div>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link link-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Vīriešiem
+            </a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Aksesuāri</a></li>
+                <li><a class="dropdown-item" href="#">Apavi</a></li>
+                <li><a class="dropdown-item" href="#">Apģērbs</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Citi</a></li>
+            </ul>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link link-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Bērniem
+            </a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Aksesuāri</a></li>
+                <li><a class="dropdown-item" href="#">Apavi</a></li>
+                <li><a class="dropdown-item" href="#">Apģērbs</a></li>
+                <li><a class="dropdown-item" href="#">Rotaļlietas</a></li>
+                <li><a class="dropdown-item" href="#">Skolai</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Citi</a></li>
+            </ul>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link link-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Mājai
+            </a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Dekori</a></li>
+                <li><a class="dropdown-item" href="#">Grāmatas</a></li>
+                <li><a class="dropdown-item" href="#">Interjera priekšmeti</a></li>
+                <li><a class="dropdown-item" href="#">Mēbeles</a></li>
+                <li><a class="dropdown-item" href="#">Remontam</a></li>
+                <li><a class="dropdown-item" href="#">Tehnika</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Citi</a></li>
+            </ul>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link link-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dārzam
+            </a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Instrumenti</a></li>                        
+                <li><a class="dropdown-item" href="#">Siltumnīcai</a></li>
+                <li><a class="dropdown-item" href="#">Stādi</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Citi</a></li>
+            </ul>
+        </li>
+    </ul>
+
+    <div class="col-md-3 text-end">
+    <button type="button" class="btn btn-link link-dark text-decoration-none me-2">Kontakti</button>
+    <button type="button" class="btn btn-dark me-4">Ieiet</button>
     </div>
 </nav>
