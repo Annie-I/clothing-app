54155c21d19f1d0753eb20b7addf49b29f3c5ea6
Annie-I <annija.iljina@gmail.com>
2021-12-30 18:00:38 +0200

    edited user public profile view

--- resources/views/components/public-profile.blade.php
+++ resources/views/components/public-profile.blade.php
@@ -6,41 +6,57 @@
 @endif
 <div class="card">
     <div class="card-body">
-        <h2 class="card-title fs-3">
-        {{$user->first_name}} {{$user->last_name}}
-        {{-- add / remove from favorite list --}}
-            @if ($user->id !== Auth::id() )
-                @if ($isFavorited)
-                    <form action="/user/{{$user->id}}/remove-from-favorites" method="post">
-                        @csrf
-                        <button type="submit" class="btn btn-outline-danger">- Dzēst favorītu</button>
-                    </form>
-                @else
-                    <form action="/user/{{$user->id}}/add-to-favorites" method="post">
-                        @csrf
-                        <button type="submit" class="btn btn-outline-primary">+ Pievienot favorītiem</button>
-                    </form>
+        <div class="row">
+            <h2 class="card-title fs-3 col-auto">{{$user->first_name}} {{$user->last_name}}</h2>
+            {{-- add / remove from favorite list --}}
+            <div class="col-auto">
+                @if ($user->id !== Auth::id() )
+                    @if ($isFavorited)
+                        <form action="/user/{{$user->id}}/remove-from-favorites" method="post">
+                            @csrf
+                            <button type="submit" class="btn btn-outline-danger">- Dzēst favorītu</button>
+                        </form>
+                    @else
+                        <form action="/user/{{$user->id}}/add-to-favorites" method="post">
+                            @csrf
+                            <button type="submit" class="btn btn-outline-primary">+ Pievienot favorītiem</button>
+                        </form>
+                    @endif
                 @endif
-            @endif
-        </h2>
+            </div>
+        </div>
         {{-- User location --}}
-        @if ($user->location )
-            <p class="fs-6">Atrašanās vieta: {{ $user->location }}</p>
-        @endif
-        {{-- Button to view user ads if there are any--}}
-        @if ($itemCount > 0)
-            <p><a href="/user/{{$user->id}}/active-items" class="btn btn-primary me-4 mt-2">Lietotāja sludinājumi</a></p>
-        @else
-            <p class="">Šim lietotājam nav aktīvu sludinājumu.</p>
-        @endif
-        <p class="fs-5 mt-3">Vidējais novērtējums: <span class="filler_text">3/5</span></p>
-        {{-- Button to view user reviews if there are any--}}
-        <p><a href="#" class="btn btn-primary me-4">Par lietotāju atstātās atsauksmes</a></p>
-        @if (Auth::user()->is_admin && Auth::user() !== $user)
-            <form method="post" action="{{route('user.block', $user->id)}}">
-                @csrf
-                <button type="submit" class="btn btn-danger">Bloķēt lietotāju</p>
-            </form>
-        @endif
+        <div class="col-auto">
+            @if ($user->location )
+                <p>Atrašanās vieta: {{$user->location}}</p>
+            @else
+                <p>Atrašanās vieta nav norādīta.</p>
+            @endif
+        </div>
+        <p class="fs-5 mt-3">Lietotāja vidējais novērtējums: <span class="filler_text">3/5</span></p>
+        <div class="row">
+            {{-- Button to add a user review if there has been comunication between them.
+                If there is a review already, user can see a button to edit his review
+                <a href="#" class="btn btn-secondary m-2 col-auto">Labot manu atsauksmi</a>
+            --}}
+            @if (Auth::user() !== $user)
+                <a href="#" class="btn btn-success m-2 col-auto">Atstāt atsauksmi par lietotāju</a>
+            @endif
+            {{-- Button to view user reviews if there are any--}}
+            <a href="#" class="btn btn-primary m-2 col-auto">Par lietotāju atstātās atsauksmes</a>
+            {{-- Button to view user ads if there are any--}}
+            @if ($itemCount > 0)
+                <a href="/user/{{$user->id}}/active-items" class="btn btn-primary m-2 col-auto">Apskatīt lietotāja sludinājumus</a>
+            @else
+                <p class="col-auto">Šim lietotājam nav aktīvu sludinājumu.</p>
+            @endif
+            {{-- Button for admin to block the user --}}
+            @if (Auth::user()->is_admin && Auth::user() !== $user)
+                <form method="post" action="{{route('user.block', $user->id)}}" class="col-auto">
+                    @csrf
+                    <button type="submit" class="btn btn-danger">Bloķēt lietotāju</p>
+                </form>
+            @endif
+        </div>
     </div>
 </div>
