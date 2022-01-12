77dec7fba186df6ac59c4dfaaa079f2a39a40d35
Annie-I <annija.iljina@gmail.com>
2022-01-12 11:39:54 +0200

    small mailbox update

--- resources/views/components/mailbox.blade.php
+++ resources/views/components/mailbox.blade.php
@@ -1,24 +1,23 @@
 {{-- Show user message list --}}
 <div class="row">
     @if (count($userMessages) === 0)
-        <p>Nav atrasts neviens sludinājums.</p>
+        <p>Nav atrasta neviena ziņa.</p>
     @endif
     @foreach ($userMessages as $message)
-        <div>
+        <ul class="mb-0">
             <li class="list-group-item mb-1">
                 <p>
-                @if (!$message->read_at && $message->receiver_id === Auth::id())
-                    <span class="badge bg-secondary">Nelasīts</span>
-                @endif
-                @if ($message->sender_id === Auth::id())
-                    Kam: {{$message->receiver->first_name}} {{$message->receiver->last_name}}
-                @else
-                    No: {{$message->sender->first_name}} {{$message->sender->last_name}}
-                @endif
-
+                    @if (!$message->read_at && $message->receiver_id === Auth::id())
+                        <span class="badge bg-secondary">Nelasīts</span>
+                    @endif
+                    @if ($message->sender_id === Auth::id())
+                        Kam: {{$message->receiver->first_name}} {{$message->receiver->last_name}}
+                    @else
+                        No: {{$message->sender->first_name}} {{$message->sender->last_name}}
+                    @endif
                 </p>
                 <a href="/message/{{$message->id}}/read" class="fw-bold text-truncate">{{$message->title}}</a>
             </li>
-        </div>
+        </ul>
     @endforeach
 </div>
